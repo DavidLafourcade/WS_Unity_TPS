ebbba776b7aea7b66f7eea21ab16fdeac1477600
David Lafourcade <dlafourcade@arfx.fr>
2020-10-09 11:26:49 +0200

    First commit

---
+++ 3D_Platformer_WS_2.0/Assets/Scripts/Gun.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Diagnostics;
+
+using Debug = UnityEngine.Debug;
+
+public class Gun : MonoBehaviour
+{
+    public float damage = 10f;
+    public float range = 100f;
+
+    public Camera TPScam;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Shoot();
+        }
+    }
+
+    void Shoot ()
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(TPScam.transform.position, TPScam.transform.forward, out hit, range))
+        {
+
+            Debug.Log(hit.transform.name);
+        }
+    }
+}
